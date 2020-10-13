3134daeeec421c7308eaa85423118c0e10034fb1
Sabrina <66120618+alsurdeabril@users.noreply.github.com>
2020-10-13 07:28:43 -0300

    Add files via upload

---
+++ Motor/Motor.java
@@ -0,0 +1,71 @@
+package ar.edu.unlam.pb1.tp04.dominio;
+
+/* 5. Definí una clase Motor considerando los siguientes atributos de clase: tipoBomba (int),
+tipoFluido (String), combustible (String). Definí un constructor asignando unos valores de
+defecto a los atributos y los métodos para poder establecer y obtener los valores de los
+atributos. Crea un método tipo procedimiento denominado dimeTipoMotor() donde a
+través de un condicional switch hagas lo siguiente:
+a. Si el tipo de motor es 0, mostrar un mensaje por consola indicando “No hay
+establecido un valor definido para el tipo de bomba”.
+b. Si el tipo de motor es 1, mostrar un mensaje por consola indicando “La bomba es
+una bomba de agua”.
+c. Si el tipo de motor es 2, mostrar un mensaje por consola indicando “La bomba es
+una bomba de gasolina”.
+d. Si el tipo de motor es 3, mostrar un mensaje por consola indicando “La bomba es
+una bomba de hormigón”.
+e. Si el tipo de motor es 4, mostrar un mensaje por consola indicando “La bomba es
+una bomba de pasta alimenticia”.
+f. Si no se cumple ninguno de los valores anteriores mostrar el mensaje “No existe
+un valor válido para tipo de bomba”. */
+
+public class Motor {
+	
+	/* Atributos propios de la clase que reflejan el estado de la clase Motor */
+	
+	public static int tipoBomba;
+	public static String tipoFluido;
+	public static String combustible;
+	public static int tipoMotor = 0; // static -> la información contenida en estos atributos va a ser común a todos los objetos, pertenece a esta clase
+	
+	/* Constructor: Le asignamos valores a los atributos de ese objeto y los inicializamos */
+	public Motor() {
+		this.tipoBomba = 0;
+		this.tipoFluido = "";
+		this.combustible = "";
+	} // end Constructor
+	
+	public void setTipoBomba(int tipoBomba) {
+		this.tipoBomba = tipoBomba;
+	} // end setTipoBomba
+	
+	public int getTipoBomba() {
+		return this.tipoBomba;
+	} // end getTipoBomba
+	
+	public void setTipoFluido(String tipoFluido) {
+		this.tipoFluido = tipoFluido;
+	} // end setTipoFluido
+	
+	public String getTipoFluido() {
+		return this.tipoFluido;
+	} // end getTipoFluiod
+	
+	public void setCombustible(String combustible) {
+		this.combustible = combustible;
+	} // end setCombustible
+	
+	public String getCombustible() {
+		return this.combustible;
+	} // end getCombustible
+	
+	/* Método a utilizar en caso que no se haya establecido un tipo de bomba: 
+	 
+	public int dimeTipoMotor(int tipoMotor) {
+		return this.tipoMotor = tipoMotor;
+	} // end dimeTipoMotor    */
+	
+	public int dimeTipoMotor() {
+        return this.tipoMotor = tipoBomba; 
+	} // end dimeTipoMotor
+	
+} // end Motor
---
+++ Motor/PruebaMotor.java
@@ -0,0 +1,67 @@
+package ar.edu.unlam.pb1.tp04.prueba;
+import ar.edu.unlam.pb1.tp04.dominio.Motor;
+
+public class PruebaMotor {
+
+	public static void main(String[] args) {
+		
+		Motor sX555 = new Motor(); // Instanciación del objeto
+		
+		/* Establecemos el tipo de bomba */
+		
+		sX555.setTipoBomba(1);
+		
+		/* Informamos tipo de bomba */
+		
+		System.out.println("El motor SX555 tiene un tipo de bomba número: " + sX555.getTipoBomba());
+		
+		/* Establecemos el tipo de combustible */
+		
+		sX555.setCombustible("Bio-diésel");
+		
+		/* Informamos tipo de combustible */
+		
+		System.out.println("El motor tiene un combustible de tipo: " + sX555.getCombustible());
+		
+		/* Establecemos el tipo de fluido */
+		
+		sX555.setTipoFluido("Aceite para caja de cambios.");
+		
+		/* Informamos el tipo de fluido */
+		
+		System.out.println("El tipo de fluido que tiene el motor es: " 
+				+ sX555.getTipoFluido());
+		
+		/* A través del condicional múltiple indicamos el tipo de motor en base al número de bomba previamente ingresado */
+		
+		sX555.dimeTipoMotor();
+		
+		switch (Motor.tipoMotor) {
+		
+		case 0: 
+			System.out.println("No ha establecido un valor definido para el tipo de bomba.");
+			break;
+			
+		case 1: 
+			System.out.println("La bomba es una bomba de agua.");
+			break;
+			
+		case 2: 
+			System.out.println("La bomba es una bomba de gasolina.");
+			break;
+			
+		case 3: 
+			System.out.println("La bomba es una bomba de hormigón.");
+			break;
+		
+		case 4:
+			System.out.println("La bomba es una bomba de pasta alimenticia.");
+			break;
+			
+		default:
+			System.out.println("No existe un valor válido para tipo bomba");
+		} // end Switch
+
+	} // end main
+
+} // end PruebaMotor
